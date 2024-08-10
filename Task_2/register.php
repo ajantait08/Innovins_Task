8e8cd51ee9bcbe8beeb6367133e736d0c6743bab
ajantait08 <53261869+ajantait08@users.noreply.github.com>
2024-08-10 15:07:11 +0530

    Update register.php

--- Task_2/register.php
+++ Task_2/register.php
@@ -168,7 +168,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <button class="btn btn-primary" type="submit">Submit</button>
               </form>
 
-              <p class="text-center text-muted mt-5 mb-0" style="font-weight:bold">Have already an account? <a href="login.php"
+              <p class="text-center text-muted mt-5 mb-0" style="font-weight:bold">Have already an account? <a href="index.php"
                     class="fw-bold text-body"><u>Login here</u></a></p>
 
             </div>
